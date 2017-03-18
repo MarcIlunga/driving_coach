fa0e6e8f604e29876a005b71372fa57e0e858d76
mrRosset <marius.rosset@epfl.ch>
2017-03-18 12:13:49 +0100

    add logs in data analysis

--- DrivingCoach/app/src/main/java/starthack/drivingcoach/DataAnalysis.java
+++ DrivingCoach/app/src/main/java/starthack/drivingcoach/DataAnalysis.java
@@ -1,6 +1,7 @@
 package starthack.drivingcoach;
 
 import com.here.android.mpa.common.RoadElement;
+import android.util.Log;
 
 /**
  * Created by marius on 18.03.2017.
@@ -8,6 +9,10 @@ import com.here.android.mpa.common.RoadElement;
 
 public class DataAnalysis {
     public static void analyse(double averageSpeed, RoadElement road){
-
+        Log.d("<<<<<", "average speed: " + averageSpeed);
+        if(road != null){
+            Log.d("<<<<<", "road name: " + road.getRoadName());
+            Log.d("<<<<<", "road speed limit: " + road.getSpeedLimit());
+        }
     }
 }
