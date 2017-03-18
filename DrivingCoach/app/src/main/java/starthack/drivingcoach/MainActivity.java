d93e83c2441fa180119581fe881f0863646f1149
mrRosset <marius.rosset@epfl.ch>
2017-03-18 22:59:18 +0100

    Workin on the bluetooth part

--- DrivingCoach/app/src/main/java/starthack/drivingcoach/MainActivity.java
+++ DrivingCoach/app/src/main/java/starthack/drivingcoach/MainActivity.java
@@ -33,6 +33,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        BleManager blmanager = new BleManager(this);
     }
 
     void onFabClick (View view) {
